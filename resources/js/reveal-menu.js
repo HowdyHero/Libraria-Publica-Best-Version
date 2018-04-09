dabd4864c34937b1abe51278edc07b11a7c36bba
Tudor Marian <howdyhero@yahoo.com>
2018-04-09 17:49:37 +0100

    pushing to github

---
+++ resources/js/reveal-menu.js
@@ -0,0 +1,15 @@
+/*jslint browser: true*/
+/*global $, jQuery*/
+/*jshint unused:false*/
+
+$(document).ready(function () {
+    
+    'use strict';
+    
+    $('#menu-btn').click(function(){
+        $('.side-menu').toggleClass('reveal');
+    });
+    
+    
+    
+});
